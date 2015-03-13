625f85c0e40caa0a6407ef3725277868641d48e2
auraltension <auraltension@riseup.net>
2015-03-13 15:03:52 +1100

    enough for today

--- implant.sh
+++ implant.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-PWN_SHELL=$(echo $0 | awk -F "[/|-]" '{print $NF}')
+#PWN_SHELL=$(finger $USER | awk -F "[/|-]" '{print $NF}')
 
 if [ "$(uname)" == "Darwin" ]; then
-  echo 'osascript -e "set volume 5"; say -v Whisper "You left me unlocked so I snuck in. powned."; say -v Hysterical "mwa ha ha ha ha ha haaa"' >> ${HOME}/.${PWN_SHELL}rc
+  echo 'osascript -e "set volume 5"; say -v Whisper "You left me unlocked so I snuck in. powned."; say -v Hysterical "mwa ha ha ha ha ha haaa"' >> ${HOME}/.profile
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  echo 'echo "Got off lucky!"' >> ${HOME}/.${PWN_SHELL}rc
+  if [ -e "${HOME}/.bashrc" ]; then
+    echo 'echo "Lock your pc!"' >> ${HOME}/.bashrc
+  fi
 else
   echo "You got off lucky!. Remember to lock your pc."
 fi
